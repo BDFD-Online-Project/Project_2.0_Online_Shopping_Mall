e0ba5ee4a8733d03ccbe09a9e434477b963c5faa
Kelvin Chen <bdfd2005@gmail.com>
2020-03-20 13:22:19 -0400

    init set up root page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
-
+import { message, Button } from 'antd';
 export default class App extends Component {
+  handleClick = () => {
+    message.success('This is a success message');
+  };
   render() {
-    return <div>App</div>;
+    return (
+      <Button type='primary' onClick={this.handleClick}>
+        Primary
+      </Button>
+    );
   }
 }
