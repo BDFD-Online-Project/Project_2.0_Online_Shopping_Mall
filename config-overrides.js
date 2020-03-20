e8f2b64b16bacb37bf82b39b1692691cdc5a90dc
Kelvin Chen <bdfd2005@gmail.com>
2020-03-20 13:58:57 -0400

    add package

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,14 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
   //正对antd实现按需打包;根据import 来打包
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: 'css'
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { '@primary-color': '#1DA57A' }
   })
 );
