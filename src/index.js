1e3339d055bb8de7f67ea800a2077e8090d6cd31
BDFD <bdfd2005@gmail.com>
2020-03-29 09:47:20 +0800

    rebuild project

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import 'antd/dist/antd.css';
-
-import App from './App';
-
-//将App组件标签渲染到index页面的div上
-ReactDOM.render(<App />, document.getElementById('root'));
